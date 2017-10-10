0a2beaa5702baf228d84703d7269f0707caff84e
gauravano <sachdeva.gaurav1997@gmail.com>
2017-10-11 00:19:21 +0530

    google map partial

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -6,4 +6,22 @@ class Userdetail < ActiveRecord::Base
   validates :numslots, presence: true
   validates :numvacant, presence: true
   validates :mobilenum, presence: true
+
+  # geocoded_by :address, :latitude  => :lat, :longitude => :lon
+  # geocoded_by :address
+  # after_validation :geocode
+  #
+  # # def address userid
+  # #    Userdetail.where(user_id: userid)[0].address
+  # # end
+  #
+  # def getlatitude userid
+  #    Userdetail.where(user_id: userid)[0].latitude
+  # end
+  #
+  # def getlongitude userid
+  #    Userdetail.where(user_id: userid)[0].longitude
+  # end
+
+
 end

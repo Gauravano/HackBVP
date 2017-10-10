34c847d2b8c17cc0f328d9fe50011d561aafd302
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2017-10-10 19:02:56 +0530

    home pages added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
+  	redirect_to '/userdetails/new'
+  end
+
+  def homepage
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :prices
   resources :userdetails
   devise_for :users
-  root 'home#index'
+  root 'home#homepage'
+  get '/home/index' => "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
